5702f0b9336e99d763c2f94b348c69b2caead797
tinaaggarwal <tina.14a@gmail.com>
2019-04-12 20:50:49 -0700

    Add files via upload

---
+++ Lab 7.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# download data set: https://drive.google.com/file/d/13nw-uRXPY8XIZQxKRNZ3yYlho-CYm_Qt/view
+# info: https://archive.ics.uci.edu/ml/datasets/banknote+authentication
+
+# load data
+bankdata = pd.read_csv("./bill_authentication.csv")
+
+# see the data
+bankdata.shape
+
+# see head
+bankdata.head()  
+
+# data processing
+X = bankdata.drop('Class', axis=1)  
+y = bankdata['Class']  
+
+from sklearn.model_selection import train_test_split  
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)  
+
+# train the SVM
+from sklearn.svm import SVC  
+svclassifier = SVC(kernel='linear')  
+svclassifier.fit(X_train, y_train) 
+
+# predictions
+y_pred = svclassifier.predict(X_test) 
+
+# Evaluate model
+from sklearn.metrics import classification_report, confusion_matrix  
+print('Confusion Matrix of Linear Kernel')
+print(confusion_matrix(y_test,y_pred))  
+
+print('Classification Report of Linear Kernel')
+print(classification_report(y_test,y_pred))
+
+def import_iris():
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+
+    # Assign colum names to the dataset
+    colnames = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'Class']
+
+    # Read dataset to pandas dataframe
+    irisdata = pd.read_csv(url, names=colnames) 
+
+    # process
+    X = irisdata.drop('Class', axis=1)  
+    y = irisdata['Class']  
+
+    # train
+    from sklearn.model_selection import train_test_split  
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+
+def polynomial_kernel():
+    # TODO
+    # NOTE: use 8-degree in the degree hyperparameter. 
+    # Trains, predicts and evaluates the model
+    from sklearn.svm import SVC 
+    svclassifier = SVC(kernel='poly', degree=8)  
+    svclassifier.fit(X_train, y_train) 
+    # predictions
+    y_pred = svclassifier.predict(X_test) 
+    # Evaluate model
+    from sklearn.metrics import classification_report, confusion_matrix  
+    print('Confusion Matrix of Polynomial Kernel')
+    print(confusion_matrix(y_test,y_pred))
+    print('Classification Report of Polynomial Kernel')
+    print(classification_report(y_test,y_pred))
+
+def gaussian_kernel():
+    # TODO
+    # Trains, predicts and evaluates the model
+    from sklearn.svm import SVC 
+    svclassifier = SVC(kernel='rbf')  
+    svclassifier.fit(X_train, y_train) 
+    # predictions
+    y_pred = svclassifier.predict(X_test) 
+    # Evaluate model
+    from sklearn.metrics import classification_report, confusion_matrix  
+    print('Confusion Matrix of Gaussian Kernel')
+    print(confusion_matrix(y_test,y_pred))
+    print('Classification Report of Gaussian Kernel')
+    print(classification_report(y_test,y_pred))
+
+def sigmoid_kernel():
+    # TODO
+    # Trains, predicts and evaluates the model
+    from sklearn.svm import SVC 
+    svclassifier = SVC(kernel='sigmoid')  
+    svclassifier.fit(X_train, y_train) 
+    # predictions
+    y_pred = svclassifier.predict(X_test) 
+    # Evaluate model
+    from sklearn.metrics import classification_report, confusion_matrix  
+    print('Confusion Matrix of Sigmoid Kernel')
+    print(confusion_matrix(y_test,y_pred))
+    print('Classification Report of Sigmoid Kernel')
+    print(classification_report(y_test,y_pred))
+
+def test():
+    import_iris()
+    polynomial_kernel()
+    gaussian_kernel()
+    sigmoid_kernel()
+
+test()
